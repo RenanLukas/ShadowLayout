7950df93f8affa9cc6b51cca27d63d845cc60b94
Luiz Filho <luiz.filho@santander.com.br>
2020-05-28 14:38:36 -0300

    Initials commit

---
+++ shadowlayout/src/main/kotlin/br/com/shadowlayout/ShadowConstraintLayout.kt
@@ -0,0 +1,12 @@
+package br.com.shadowlayout
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class ShadowConstraintLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr)
