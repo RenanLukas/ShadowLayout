51f68d33d950f7ce6e026bf51e9ac42b89469f76
Luiz Filho <luiz.filho@santander.com.br>
2020-05-30 19:19:19 -0300

    fix animation and matrix translate linear gradient in shader

--- shadowlayout/src/main/kotlin/br/com/shadowlayout/ShadowConstraintLayout.kt
+++ shadowlayout/src/main/kotlin/br/com/shadowlayout/ShadowConstraintLayout.kt
@@ -1,12 +1,9 @@
 package br.com.shadowlayout
 
-import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
-import android.view.View
-import android.view.animation.LinearInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.children
 import br.com.shadowlayout.drawable.ShadowDrawable
@@ -17,8 +14,6 @@ class ShadowConstraintLayout @JvmOverloads constructor(
     attrs: AttributeSet,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-    private val path = Path()
-    private val paint = Paint()
     private val shadowDrawable = ShadowDrawable()
 
     init {
@@ -30,11 +25,6 @@ class ShadowConstraintLayout @JvmOverloads constructor(
         return super.verifyDrawable(who) || who === shadowDrawable
     }
 
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
-//        shadowDrawable.setBounds(0, 0, width, height)
-    }
-
     override fun dispatchDraw(canvas: Canvas?) {
         // call block() here if you want to draw behind children
         super.dispatchDraw(canvas)
@@ -42,36 +32,9 @@ class ShadowConstraintLayout @JvmOverloads constructor(
 
         children.forEach { view ->
             canvas?.let {
+                shadowDrawable.setBounds(view.left, view.top, view.right, view.bottom)
                 shadowDrawable.draw(it)
             }
         }
-    }
-
-    private fun animationBackground(view: View, canvas: Canvas?) {
-        val animator =
-            ValueAnimator.ofFloat(canvas!!.width.toFloat(), 0f, canvas.width.toFloat())
-
-        with(animator) {
-            duration = 1000
-            interpolator = LinearInterpolator()
-            repeatCount = ValueAnimator.INFINITE
-            addUpdateListener {}
-        }
-    }
-
-    private fun updateShader(width: Float, factor: Float = -1F) {
-        val left = width * factor
-        val shader = LinearGradient(
-            left, 0F, left + width, 0F,
-            intArrayOf(
-                Color.BLACK,
-                Color.GRAY,
-                Color.BLACK
-            ),
-            floatArrayOf(0f, 0.5f, 1f),
-            Shader.TileMode.CLAMP
-        )
-        paint.shader = shader
-
     }
 }
--- shadowlayout/src/main/kotlin/br/com/shadowlayout/drawable/ShadowDrawable.kt
+++ shadowlayout/src/main/kotlin/br/com/shadowlayout/drawable/ShadowDrawable.kt
@@ -1,16 +1,19 @@
 package br.com.shadowlayout.drawable
 
 import android.animation.ValueAnimator
-import android.graphics.Canvas
-import android.graphics.ColorFilter
-import android.graphics.PixelFormat
-import android.graphics.Rect
+import android.graphics.*
 import android.graphics.drawable.Drawable
 
 class ShadowDrawable : Drawable() {
+    val matrix = Matrix()
+    private val path = Path()
+    private val paint = Paint().apply {
+        //        xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+    }
 
-    val aniamtion = ValueAnimator.ofFloat(0f, 1f).apply {
-        duration = 1000
+    val animation = ValueAnimator.ofFloat(0f, 1f).apply {
+        duration = 1000L
+        startDelay = 100L
         repeatMode = ValueAnimator.RESTART
         repeatCount = ValueAnimator.INFINITE
         addUpdateListener {
@@ -18,14 +21,53 @@ class ShadowDrawable : Drawable() {
         }
     }
 
+    private fun updateShader(width: Float, height: Float, factor: Float = -1F) {
+        val left = width * factor
+        val shader = LinearGradient(
+            left, 0F, left + width, 0F,
+            intArrayOf(
+                Color.GREEN,
+                Color.BLACK,
+                Color.GREEN
+            ),
+            floatArrayOf(0.25f, 0.5f, 1f),
+            Shader.TileMode.CLAMP
+        )
+        paint.shader = shader
+    }
+
     override fun draw(canvas: Canvas) {
-        aniamtion.start()
+        val valueAnimator = animation.animatedFraction
+        val width = bounds.right.toFloat()
+        val height = bounds.bottom.toFloat()
+
+        matrix.reset()
+        matrix.postTranslate((width*2)*valueAnimator, 0f)
+        paint.shader.setLocalMatrix(matrix)
+
+        canvas.drawPath(path, paint)
+        if (!animation.isStarted) {
+            animation.start()
+        }
     }
 
     override fun onBoundsChange(bounds: Rect?) {
         super.onBoundsChange(bounds)
-        val width = bounds?.width()
-        val height = bounds?.height()
+        bounds?.let {
+            val width = bounds.right.toFloat()
+            val height = bounds.bottom.toFloat()
+            //remove after
+            path.addRect(
+                RectF(
+                    bounds.left.toFloat(),
+                    bounds.top.toFloat(),
+                    width,
+                    height
+                ),
+                Path.Direction.CW
+            )
+            updateShader(width = width, height = height)
+        }
     }
 
     override fun setAlpha(alpha: Int) {
