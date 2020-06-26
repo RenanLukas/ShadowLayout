645e7ce5312d01ae3d4ed5e4fd005f4cb83e0ca1
Luiz Filho <luiz.filho@santander.com.br>
2020-06-26 00:15:03 -0300

    commit 1

---
+++ changelog.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+function check_changelog() {
+  if [[ ! -f ./CHANGELOG.md ]]; then
+    touch CHANGELOG.md
+  fi
+}
+
+
+function get_changelog() {
+  local currentTag previousTag prevChangelogContents
+  currentTag=$(git describe --abbrev=0 --tags)
+  previousTag=$(git describe --abbrev=0)
+  prevChangelogContents=$(cat ./CHANGELOG.md)
+
+  {
+    echo "## $currentTag";
+    echo "";
+#    echo $(git shortlog --no-merges | grep -v "Upgrade to";)
+    git log --pretty="%ad %h - %s %n" --decorate --color --date=format:'%Y/%m/%d'
+    echo "";
+  } > CHANGELOG.md
+  echo "$prevChangelogContents" >> CHANGELOG.md
+}
+
+function main() {
+  check_changelog
+  get_changelog
+}
+
+main
